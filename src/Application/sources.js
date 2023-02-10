97667ed608dd1a70c8caf12cb6da75799dfd85e7
Davide <davidebarca69@gmail.com>
2023-02-10 12:09:50 +0100

    Define objects to load

---
+++ src/Application/sources.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "baked",
+        type: "texture",
+        path: "/model/baked.jpg"
+    },
+    {
+        name: "model",
+        type: "gltf",
+        path: "/model/coffeeBar.glb"
+    }
+]
