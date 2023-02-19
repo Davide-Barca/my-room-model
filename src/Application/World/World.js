866d932f028b6ddb6f5c632ffd7005dd33fc0e5f
Davide <davidebarca69@gmail.com>
2023-02-19 16:47:31 +0100

    Add monitor screen video

--- src/Application/World/World.js
+++ src/Application/World/World.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 
 import Application from '../Application.js'
+import Screen from './Screen.js'
 
 export default class World{
     constructor(){
@@ -8,6 +9,10 @@ export default class World{
         this.application = new Application()
         this.scene = this.application.scene
         this.resources = this.application.resources
+        this.time = this.application.time
+
+        this.model = {}
+
 
         this.resources.on('loaded', () => {
             this.setMaterial()
@@ -18,32 +23,62 @@ export default class World{
     setMaterial(){
         // Setup Backed Textures
         this.baked = {}
-        this.lights = {}
+        this.macbook = {}
 
         // Setup Baked Texture
         this.baked.texture = this.resources.items.baked
         this.baked.texture.flipY = false
         this.baked.texture.encoding = THREE.sRGBEncoding
 
+        this.macbook.texture = this.resources.items.macbookDesktop
+        // this.macbook.texture.flipY = false
+        this.macbook.texture.encoding = THREE.sRGBEncoding
+
 
         // Setup Materials
         this.baked.material = new THREE.MeshBasicMaterial({ map: this.baked.texture })
-        this.lights.material = new THREE.MeshBasicMaterial({ color: "#FFFEA6" })
+        this.macbook.material = new THREE.MeshBasicMaterial({ map: this.macbook.texture })
     }
 
     setModel(){
-        this.model = {}
         this.model.scene = this.resources.items.model.scene
         this.model.scene.rotation.y = - (Math.PI / 2)
-        console.log(this.model.scene)
+        this.model.scene.position.z = 20
         // Select scene children
-        this.model.baked = this.model.scene.children.find((child) => child.name === 'Model')
-        this.model.lights = this.model.scene.children.find((child) => child.name === 'Lights')
+        this.model.baked = this.model.scene.children.find((child) => child.name === 'model')
+        this.model.chair = this.model.scene.children.find((child) => child.name === 'chair')
+        this.model.left = this.model.scene.children.find((child) => child.name === 'monitorLeftScreen')
+        this.model.right = this.model.scene.children.find((child) => child.name === 'monitorRightScreen')
+        this.model.macbook = this.model.scene.children.find((child) => child.name === 'macbookScreen')
+
+        console.log(this.model);
 
         // Assign materials to children
         this.model.baked.material = this.baked.material
-        this.model.lights.material = this.lights.material
+        this.model.chair.material = this.baked.material
+        this.model.macbook.material = this.macbook.material
+
+
+
+        // Set Video Screen
+        this.screenRight = new Screen(
+            this.model.right,
+            '/model/blender.mp4'
+        )
+        this.screenLeft = new Screen(
+            this.model.left,
+            '/model/code.mp4'
+        )
+
+        // this.model.lights.material = this.lights.material
 
         this.scene.add(this.model.scene)
+    }
+
+    update()
+    {
+        if(this.model.chair){
+            this.model.chair.rotation.y = Math.sin(this.time.elapsed * 0.0005) * 0.5
+        }
     }
 }
